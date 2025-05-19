bca24be225a2c79a58d3c4125188c92cbe3eb630
Fae <109899113+iamf-a-e@users.noreply.github.com>
2025-05-19 09:50:15 +0200

    main.py

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ wa_token = os.environ.get("WA_TOKEN") # WhatsApp API Key
 gen_api = os.environ.get("GEN_API")  # Gemini API Key
 owner_phone = os.environ.get("OWNER_PHONE") # Owner's phone number with country code
 owner_phone_1 = os.environ.get("OWNER_PHONE_1")
-owner_phone_1 = os.environ.get("OWNER_PHONE_2")
-owner_phone_1 = os.environ.get("OWNER_PHONE_3")
-owner_phone_1 = os.environ.get("OWNER_PHONE_4")
+owner_phone_2 = os.environ.get("OWNER_PHONE_2")
+owner_phone_3 = os.environ.get("OWNER_PHONE_3")
+owner_phone_4 = os.environ.get("OWNER_PHONE_4")
 
 app = Flask(__name__)
 user_states = {}
