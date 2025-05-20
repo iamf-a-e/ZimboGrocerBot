505f179a6e979438517cef520771cebecb9d3471
Fae <109899113+iamf-a-e@users.noreply.github.com>
2025-05-20 07:55:27 +0200

    main.py

--- main.py
+++ main.py
@@ -440,6 +440,11 @@ def message_handler(data, phone_id):
         if prompt.lower() in ["yes", "y"]:
             send("Please enter the receiver’s full name.", sender, phone_id)
             user_data["step"] = "get_receiver_name"
+        elif prompt.lower() in ["no", "n"]:
+        send("What would you like to do next?\n- View cart\n- Clear cart\n- Remove <item>\n- Add Item", sender, phone_id)
+        user_data["step"] = "post_add_menu"  # Transition back to the post-add menu
+        else:
+        send("Please respond with 'yes' or 'no'.", sender, phone_id)
 
 
     elif step == "post_add_menu":
