fcc1a1eaefc29d9685f0faf97d606c27956e8f2f
Fae <109899113+iamf-a-e@users.noreply.github.com>
2025-05-21 22:33:16 +0200

    main.py

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ REDIS_URL = os.getenv('REDIS_URL')
 redis_client = redis.Redis(
     host=os.getenv('REDIS_HOST'),
     port=int(os.getenv('REDIS_PORT', 6379)),
-    password=os.getenv('REDIS_PASSWORD')
+    password=os.getenv('REDIS_PASSWORD'),
+    decode_responses=True,
+    ssl=True
 )
 
 # Set a value
