499e61d2070392681b6530ec51d1a07f1fe527dc
Fae <109899113+iamf-a-e@users.noreply.github.com>
2025-07-07 16:39:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ class User:
 
 # State handlers
 def handle_ask_name(prompt, user_data, phone_id):
-    send("Hello! Welcome to Zimbogrocer. What's your name?", user_data['sender'], phone_id)
+    send("Hello! Welcome to Zimbogrocer. What's your full name? e.g Mildred Moyo", user_data['sender'], phone_id)
     update_user_state(user_data['sender'], {'step': 'save_name'})
     return {'step': 'save_name'}
 
