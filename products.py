80b5ba08fd621933bdca04c2ee2b64f270c5da5e
Fae <109899113+iamf-a-e@users.noreply.github.com>
2025-07-09 08:53:17 +0200

    Update products.py

--- products.py
+++ products.py
@@ -1,8 +1,14 @@
 class Product:
-    def __init__(self, name, price, description):
+    def __init__(self, name, price, description="", stock=0):
         self.name = name
         self.price = price
         self.description = description
+        self.stock = stock
+        self.active = stock > 0  # Automatically inactive if stock is 0
+
+    def is_available(self):
+        return self.stock > 0 and self.active
+        
 
 class Category:
     def __init__(self, name):
